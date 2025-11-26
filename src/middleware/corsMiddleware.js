29a0f0829528534754988b8233246e61f092b3c3
nxiiio <w.orellana@duocuc.cl>
2025-11-25 23:43:36 -0300

    cors vercel

--- src/middleware/corsMiddleware.js
+++ src/middleware/corsMiddleware.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://localhost:3000',
+    'https://level-up-tienda-gamer-w-react.vercel.app'
 ]
 
 const corsOptions = {
